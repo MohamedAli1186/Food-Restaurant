5b1b12b0017ee79c04d633700d70dd925543a562
MohamedAli1186 <Mohammedaallii10@gmail.com>
2024-09-12 16:23:46 +0300

    continue Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { BookTableComponent } from './book-table/book-table.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'main', component: MainComponent} ,
+    {path:'menu', component: MenuComponent} ,
+    {path:'book-table', component: BookTableComponent} ,
+    {path:'contact', component: ContactComponent} ,
+    { path: '**', redirectTo: '/main' },
+];

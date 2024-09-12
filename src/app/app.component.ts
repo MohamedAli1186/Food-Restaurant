8c95d20177c41de53904c068e2ffe4c013851324
MohamedAli1186 <Mohammedaallii10@gmail.com>
2024-09-12 16:54:37 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MainComponent } from "./main/main.component";
 import { NavComponent } from "./nav/nav.component";
@@ -8,7 +8,7 @@ import { FooterComponent } from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, NavComponent, FooterComponent],
+  imports: [RouterOutlet,RouterLink , HeaderComponent, MainComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
